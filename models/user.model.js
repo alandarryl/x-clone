ac8228691f206faaf366538a95839f902049e1a0
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-12-14 15:54:52 +0100

    complete the user model

---
+++ models/user.model.js
@@ -0,0 +1,46 @@
+
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            minlength:3,
+            maxlength:30
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            match: [/.+\@.+\..+/, 'Please fill a valid email address']
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        bio: {
+            type: String,
+            maxlength: 160,
+            default: ''
+        },
+        avatar_url: {
+            type: String,
+            default: ''
+        },
+        is_verified: {
+            type: Boolean,
+            default: false
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
+
+
