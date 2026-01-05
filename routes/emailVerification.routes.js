ee4713bc2923634f45e69f28e8eba7cd40a209ac
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-01-05 09:11:27 +0100

    adjust the verify email routes

--- routes/emailVerification.routes.js
+++ routes/emailVerification.routes.js
@@ -1,10 +1,11 @@
-
 const express = require('express');
 const router = express.Router();
-
 const emailVerificationController = require('../controllers/emailVerification.controller');
 
-// Route to handle email verification
-router.get('/verify-email', emailVerificationController.verifyEmail);
+// Route to handle email verification using token
+router.get('/verify-email/:token', emailVerificationController.verifyEmail);
+
+// Route to check if an email is verified
+router.get('/check/:email', emailVerificationController.checkVerificationStatus);
 
 module.exports = router;
